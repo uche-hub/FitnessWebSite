121088924a500436c632ff4819b7515daed9c693
UCHENNA <ucj.justice@gmail.com>
2020-10-26 09:53:42 +0100

    working on the classes page

--- JS/script.js
+++ JS/script.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
         $(".nav").toggleClass("open")
         $(this).toggleClass("active");
     })
+    $(".accordian-container").click(function(){
+        $(".accordian-container").children(".body").slideUp();
+        $(".accordian-container").removeClass("active")
+        $(".accordian-container").children(".head").children("span").removeClass("fa-angle-down").addClass("fa-angle-up")
+        $(this).children(".body").slideDown();
+        $(this).addClass("active")
+        $(this).children(".head").children("span").removeClass("fa-angle-up").addClass("fa-angle-down")
+    })
 })
